876a3e64bc50da0b82d44e86130017494e3bee3d
Joao Lara <joaovdelara@gmail.com>
2025-05-07 20:14:07 -0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import "./App.css";
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="container">
       <h1>Contador: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Adicionar</button>
       <button onClick={() => setCount(count - 1)}>Remover</button>
