8ce08ed52386d41685b5654bc3ab4779a021b4ec
Joao Lara <joaovdelara@gmail.com>
2025-05-07 20:12:56 -0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      Teste Reset
+    <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Adicionar</button>
+      <button onClick={() => setCount(count - 1)}>Remover</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;
